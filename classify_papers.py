75694d3f4fb5ce0eac9283ec3d749e84a9f3695c
nicheng0019 <zoubaozhu@126.com>
2018-04-19 13:05:09 +0800

    Add files via upload
    upodate

--- classify_papers.py
+++ classify_papers.py
@@ -4,6 +4,7 @@ import re
 import codecs
 import shutil
 import numpy as np
+import fnmatch
 from textblob import TextBlob
 #from textblob import Word
 from sklearn.feature_extraction.text import CountVectorizer
@@ -12,7 +13,9 @@ from nltk.stem.wordnet import WordNetLemmatizer
 
 from utils import *
 
-def getnormalizename(originalname):
+dict_vec = {}
+
+def get_normalize_name(originalname):
     lem = WordNetLemmatizer()
 
     pattern = '_[0-9]+.[0-9]+(.*)pdf'
@@ -39,7 +42,7 @@ def getnormalizename(originalname):
 
     return words_str
 
-def getfeaturebyfilename(filename, word_dict):
+def get_feature_by_filename(filename, word_dict):
     feature = np.zeros((1, len(word_dict.keys())))
     words = filename.strip().split(" ")
     for word in words:
@@ -48,48 +51,92 @@ def getfeaturebyfilename(filename, word_dict):
 
     return feature
 
-def classifypaperfiles(paperdir, namelistfile, num_clusters=5):
-    ftxt = codecs.open(namelistfile, encoding="utf-8", mode="w")
-
-    rootdir = paperdir
-    paper_names = []
-    for fn in os.listdir(rootdir):
-        if os.path.isdir(os.path.join(rootdir, fn)):
+def get_words_feature(words):
+    global dict_vec
+    feature = []
+    for word in words:
+        if word not in dict_vec.keys():
             continue
 
-        normal_name = getnormalizename(fn)
+        feature.append(dict_vec[word])
 
-        if normal_name != None:
-            paper_names.append(unicode(normal_name))
-            ftxt.write(normal_name)
-            ftxt.write("\r\n")
+    return np.array(feature)
 
-    ftxt.close()
+def get_paper_content(filename):
+    with codecs.open(filename, mode="r", encoding="utf-8") as ftxt:
+        content = ftxt.readlines()
 
-    count_v1 = CountVectorizer(max_df=0.8, min_df=0.01)
-    counts_train = count_v1.fit_transform(paper_names)
+    if len(content) == 0:
+        return ""
 
-    word_dict = {}
-    for index, word in enumerate(count_v1.get_feature_names()):
-        word_dict[word] = index
+    content = content[0]
+    words = content.split(" ")
+    wordsnew = []
+    for word in words:
+        word = word.lower()
+        if check_invalid_word(word):
+            continue
 
-    features = np.zeros((len(paper_names), len(word_dict.keys())))
-    for index, name in enumerate(paper_names):
-        features[index] = getfeaturebyfilename(name, word_dict)[0]
+        wordsnew.append(word)
+
+    print(wordsnew)
+
+    return wordsnew
+
+def get_paper_feature(fnpath):
+    content = get_paper_content(fnpath)
+
+    get_words_feature(content)
+
+
+
+def classify_papers(rootdir, num_clusters=5, featuretype="content"):
+    for fn in os.listdir(rootdir):
+        fnpath = os.path.join(rootdir, fn)
+        if os.path.isdir(fnpath):
+            continue
+
+        if featuretype == "name":
+            names = []
+            name = get_normalize_name(fn)
+            if name != None:
+                names.append(unicode(name))
+
+            count_v1 = CountVectorizer(max_df=0.8, min_df=0.01)
+            counts_train = count_v1.fit_transform(names)
+
+            word_dict = {}
+            for index, word in enumerate(count_v1.get_feature_names()):
+                word_dict[word] = index
+
+            features = np.zeros((len(names), len(word_dict.keys())))
+            for index, name in enumerate(names):
+                features[index] = get_feature_by_filename(name, word_dict)[0]
+
+        else:
+            features = []
+            feature = get_paper_feature(fnpath)
+
+            if feature != None:
+                features.append(feature)
+
+            features = np.array(features)
 
     km = KMeans(n_clusters=num_clusters).fit(features)
     paper_labels = km.labels_
 
     print(paper_labels)
 
+    return
     for fn in os.listdir(rootdir):
-        if os.path.isdir(os.path.join(rootdir, fn)):
+        fnpath = os.path.join(rootdir, fn)
+        if os.path.isdir(fnpath):
             continue
 
-        normal_name = getnormalizename(fn)
+        normal_name = get_normalize_name(fn)
 
         if normal_name != None:
-            feature = getfeaturebyfilename(normal_name, word_dict)
+            feature = get_feature_by_filename(normal_name, word_dict)
             predictresult = km.predict(feature)
             dstdir = os.path.join(rootdir, str(predictresult[0]))
         else:
@@ -99,8 +146,26 @@ def classifypaperfiles(paperdir, namelistfile, num_clusters=5):
             os.makedirs(dstdir)
         shutil.copy(os.path.join(rootdir, fn), os.path.join(dstdir, fn))
 
+def init_dict_vec(dictfile):
+    global dict_vec
+    with open(dictfile, "r") as fdict:
+        lines = fdict.readlines()
+
+    for line in lines:
+        datas = line.split(" ")
+        word = datas[0]
+        vec = datas[1:]
+        vec = map(eval, vec)
+
+        dict_vec[word] = np.array(vec)
+
+    print(dict_vec["and"])
+
+
 if __name__=='__main__':
-    paperdir = r"D:\paper"
-    namelistfile = "pdf_train.txt"
+    dictfile = r"resource/glove.6B.50d.txt"
+    init_dict_vec(dictfile)
+
+    filedir = r"content"
     num_clusters = 10
-    classifypaperfiles(paperdir, namelistfile, num_clusters=num_clusters)
+    classify_papers(filedir, num_clusters=num_clusters)
--- utils.py
+++ utils.py
@@ -2,12 +2,15 @@ def clean_string(srcdata):
     data = srcdata.replace("\r\n", " ")
     data = data.strip()
     data = data.replace(",", "")
+    data = data.replace(".", "")
+    data = data.replace(":", "")
     data = data.replace("(", "")
     data = data.replace(")", "")
     data = data.replace("[", "")
     data = data.replace("]", "")
     data = data.replace("+", "")
     data = data.replace("-", " ")
+    data = data.replace("/", " ")
 
     while True:
         dstdata = data.replace("  ", " ")
@@ -16,3 +19,17 @@ def clean_string(srcdata):
         data = dstdata
 
     return dstdata
+
+def check_invalid_word(word):
+    import re
+
+    if len(word) < 2:
+        return True
+
+    pattern = '^[0-9]+$'
+    if re.match(pattern, word) is not None:
+        return True
+
+    remove_words = ["page"]
+    if word in remove_words:
+        return True
